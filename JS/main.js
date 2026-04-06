3bd92dd7a3d871f40bf1601fb3ec9b59581d8823
Mohamed Kamel Atlam <mohamedatlam1710@gmail.com>
2026-04-06 22:10:25 +0200

    init commit

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+// Variables
+let hoursDom = document.getElementById("hours");
+let dateDom = document.getElementById("date");
+
+// Event
+hoursDom.innerHTML = getNowHours();
+dateDom.innerHTML = getTodayDate();
+selectDom.addEventListener("change" , function(){
+    let cityDom = document.getElementById("city");
+    cityDom.innerHTML = this.options[this.selectedIndex].text;
+
+    getPrayerTimes();
+});
